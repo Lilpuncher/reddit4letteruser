86b90cc41cc85590f0af89c78b9fc395b20a754d
Lilpuncher <ziad.cina@gmail.com>
2021-05-11 01:37:09 +0300

    Update UserFinder.py

--- UserFinder.py
+++ UserFinder.py
@@ -1,10 +1,9 @@
 import random
 import requests
 import json
-import time
+
 CHARACTERS = "abcdefghijklmnopqrstuvwxyz1234567890-_"
-#CHARACTERS[random.randint(0,63)]
-#https://www.reddit.com/api/username_available.json?user=USER
+
 while(True):
     USER = CHARACTERS[random.randint(0,37)] + CHARACTERS[random.randint(0,37)] + CHARACTERS[random.randint(0,37)] + CHARACTERS[random.randint(0,37)]
     URL = "https://www.reddit.com/api/username_available.json?user=" + USER
